3ee91fdfe98663fb21f7d525c9fbc1bbb50a57a0
Holman Plazas <holmanyiyos@gmail.com>
2021-08-26 20:00:15 -0500

    -

--- src/pages/BadgeView.js
+++ src/pages/BadgeView.js
@@ -6,19 +6,7 @@ import DeleteBadgeModal from '../components/DeleteBadgeModal';
 import Badge from '../components/Badge';
 
 
-// function useIncreaseCount(max){
-//     const [count, setCount] = React.useState(0)
-
-//     if(count > max){
-//         setCount(0)
-//     }
-
-//     return[count, setCount]
-
-// }
-
 function BadgeView(props){
-    // const [count, setCount] = useIncreaseCount(4)
     const badge = props.badge;
     return (<React.Fragment>
         <div className="BadgeDetails__hero">
@@ -47,11 +35,6 @@ function BadgeView(props){
                     <h2>Actions</h2>
                     <div>
                         <div>
-                            {/* <button  onClick={()=>{
-                                setCount(count +1)
-                            }} className="btn btn-primary mr-4">
-                                Increase Count: {count}
-                            </button> */}
                             <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>Edit</Link>
                         </div>
 
